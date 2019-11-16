91b13b7a9d2af97743299e56492cd9c3bf58cbff
dan <dan@localhost.localdomain>
2019-11-16 17:57:47 +0300

    add our results from the api key to our index view function

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_newsSource
 
 #views
 @app.route('/')
@@ -8,7 +9,13 @@ def index():
     this is a view function thats responsible for returning the index page and its data
     '''
     title = " The News Highlight apllication "
-    return render_template('index.html',title = title)
+
+    #getting the news-sources
+    news_sources = get_newsSource('sources')
+    print(news_sources)
+
+
+    return render_template('index.html',title = title,srcNews = news_sources)
 
 @app.route('/newsSrc/<int:source_id>')
 def newSource(source_id):
