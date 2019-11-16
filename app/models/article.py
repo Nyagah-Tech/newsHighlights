bb8f38c98ba86af1d6a2ea643d46bff293db031e
dan <dan@localhost.localdomain>
2019-11-17 01:38:00 +0300

    create  the view fuction for our articles and link it with our macro.html to be displayued

--- app/models/article.py
+++ app/models/article.py
@@ -2,10 +2,11 @@ class Article :
     '''
     this class defines how a particular article will be made
     '''
-    def __init__(self,name,author,title,urlImage,description,content):
+    def __init__(self,name,author,title,urlToImage,description,publishedAt,content):
         self.name = name
         self.author = author
         self.title = title
-        self.urlImage = urlImage
+        self.urlToImage = urlToImage
         self.description = description
+        self.publishedAt = publishedAt
         self.content = content
