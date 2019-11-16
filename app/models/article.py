6d744365b2418844eee42954c9e93a1a540c2186
dan <dan@localhost.localdomain>
2019-11-16 21:09:23 +0300

    create an aticle class that defines how our articles will be collected

---
+++ app/models/article.py
@@ -0,0 +1,11 @@
+class Article :
+    '''
+    this class defines how a particular article will be made
+    '''
+    def __init__(self,name,author,title,urlImage,description,content):
+        self.name = name
+        self.author = author
+        self.title = title
+        self.urlImage = urlImage
+        self.description = description
+        self.content = content
--- app/models/newSource.py
+++ app/models/newSource.py
@@ -2,8 +2,9 @@ class NewSrc:
     '''
     this is a News source class that defines the sources object.how they will be created
     '''
-    def __init__(self,name,description,category,url):
+    def __init__(self,name,description,category,url,id):
         self.name = name
         self.description = description
         self.category = category
         self.url = url
+        self.id = id
